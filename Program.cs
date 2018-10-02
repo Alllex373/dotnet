d079607285fdf4d64e714059b2f2ce189ae0540d
Axel3 <zabi-zabi-zabi@rambler.ru>
2018-10-02 20:05:22 +0300

    my commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator_bib;
+
+namespace ConsoleApp1
+{
+   public class Program
+    {
+      public static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            Console.WriteLine(calc.Count(4, 5, Calculator_bib.Action.Div));
+
+            Console.ReadLine();
+
+        }  
+    }
+}
